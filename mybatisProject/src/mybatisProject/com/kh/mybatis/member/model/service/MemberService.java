146bc2af87abb6efccacf543a503412ec25dd905
yoonyj1 <116607314+yoonyj1@users.noreply.github.com>
2023-03-20 10:48:25 +0900

    202303201048

---
+++ mybatisProject/src/mybatisProject/com/kh/mybatis/member/model/service/MemberService.java
@@ -0,0 +1,12 @@
+package mybatisProject.com.kh.mybatis.member.model.service;
+
+import mybatisProject.com.kh.mybatis.member.model.vo.Member;
+
+public interface MemberService {
+	// 인터페이스: 상수필드(묵시적으로 public static final) + 추상메소드(묵시적으로 public abstract)만 쓸 수 있음
+	/*public abstract*/ int insertMember(Member m);
+	
+	Member loginMember(Member m);
+	
+	int updateMember(Member m);
+}
